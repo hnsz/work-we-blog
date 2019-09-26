cd43e02bc11f780d6082c1c4b40c7375c486cc2a
hnsz <hnsz>
2019-09-26 19:26:49 +0200

    Permissions on storage. Backup acl

---
+++ project_files/acl.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash 
+###############
+#           ############
+#
+#	Set permissions for both webserver and developer to have rw access
+#	And make it stick
+#
+#
+Dir=../storage
+Uid=1234
+Gid=33
+
+#	sudo chown -R $Uid:$Gid $Dir;
+sudo 
