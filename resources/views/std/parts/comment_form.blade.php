38a0aa77b7fb18e10e02f09a319932cc33cb2c8b
hsec <hansrudolfw@gmail.com>
2019-10-06 12:56:40 +0200

    change form template

--- resources/views/std/parts/comment_form.blade.php
+++ resources/views/std/parts/comment_form.blade.php
@@ -8,21 +8,21 @@
 
 	
 	<div class='input-group input-group-sm'>
-
-			<label class="input-group-prepend input-group-text">Write a Comment</label>
-			<input type='text' name='minor_title' placeholder='title is optional' class='form-control' maxlength='25' value="{{old('minor_title')}}">
+	   <div class='input-group-text input-group-prepend'>
+			<label for="form[comment][item[body]]">Write a Comment</label>
+	   </div>
+	   <input type='text' name='minor_title' placeholder='title is optional' 
+		style="margin:4px 67px 4px 80px"
+		class='form-control' maxlength='25' value="{{old('minor_title')}}">
 	</div> 
 
-	 <div class='input-group input-group-sm'>
-	   <div class='input-group-prepend'>
-		   <label class="input-group-text btn btn-success"></label>
-		</div>
-		<textarea rows='4' cols='40' name='body' placeholder="" required class='form-control'>{{old('body')}}</textarea>
-		<div class="input-group-append">
-				<input type='submit' name='submit' class=' btn-success'>			
-		  </div>
-   	   </div>
+	<div class='input-group input-group-sm'>
 
-
-	</div>
+	    <textarea id="form[comment][item[body]]" rows='4' cols='40' name='body'
+		 placeholder="" required class='form-control'>{{old('body')}}</textarea>
+            <div class="input-group-append">
+            <input type='submit' name='submit' class='btn-success'>			
+            </div>
+        </div>
+    </div>
 </form>
