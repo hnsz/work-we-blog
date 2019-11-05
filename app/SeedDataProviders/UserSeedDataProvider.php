d60f98146b52793066a355ab3c22a9cfa2728208
hans <hansrudolfw@gmail.com>
2019-11-05 15:50:56 +0100

    commit I've been an idiot and used root. access rights reset

--- app/SeedDataProviders/UserSeedDataProvider.php
+++ app/SeedDataProviders/UserSeedDataProvider.php
@@ -46,7 +46,7 @@ class UserSeedDataProvider implements SeedDataProvider
                     ];
                 }
             },
-            $this->fakerGen);
+            $this->fakerGen);ls -a
             
         $this->data = iterator_to_array($generator);
     }
